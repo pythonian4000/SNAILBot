da0be3759e9b34aa6086cd138fb895f1420ff41b
Jack Grigg <me@jackgrigg.com>
2011-04-20 13:32:14 +0000

    Erk, the previous commit forgot that the channel names are stored with #s in the db.

--- cgi/out.pl
+++ cgi/out.pl
@@ -103,7 +103,7 @@ if ($channel !~ m/\A#?[.\w-]+\z/smx){
 my $count;
 {
     my $sth = $dbh->prepare_cached('SELECT COUNT(*) FROM irclog WHERE server = ? AND channel = ? AND day = ?');
-    $sth->execute($server, $channel, $date);
+    $sth->execute($server, '#' . $channel, $date);
     $sth->bind_columns(\$count);
     $sth->fetch();
     $sth->finish();
